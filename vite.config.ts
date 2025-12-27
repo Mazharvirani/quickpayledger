e6baeb5942244116a9b9bb5665d77d668ddad5e8
Mazharvirani <virani.mazher@gmail.com>
2025-12-28 00:56:06 +0500

    config issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // ✅ Add this line for Vercel deployment
   server: {
     host: "::",
     port: 8080,
